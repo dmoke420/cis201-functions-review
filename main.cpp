1f5973b5cfe57f7213c54fee1ac97ae1b5e40ed7
Lorenzo Cooper <lorenzocooper@cis.vvc.edu>
2018-10-16 20:11:12 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,59 @@
 #include<iostream>
 
+#include<string>
+
+#include<cctype>
 
 using namespace std;
+
+int a_search(string p);
+bool is_a(char a);
+
+
+
+
+
+
 
 
 int main()
 {
-    cout << "Hello World"<< endl;
 
-    return 0;
+string a;
+cout << " Please enter a string: ";
+getline(cin, a);
+cout << "This is the numbers of a: " << a_search(a) << endl;
+
+
+return 0;
 }
+
+
+int a_search(string p)
+{
+   int count = 0;
+  
+  for(int l = 0; l < p.length(); l++)
+   
+    {   
+        if(is_a(p.at(l)));
+        count++;
+    }
+
+
+
+
+return count;
+
+
+}
+
+bool is_a(char a) // char are only charcters one charcter 
+{
+
+return a == 'a' || a == 'A';
+
+}
+
+
+
